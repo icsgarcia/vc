befdf924b2410024f00c8525c739cc311af1d11d
Ivan Garcia <icsgarcia2002@gmail.com>
2025-11-07 12:49:20 +0800

    updated ui, implemented user's media (video and audio)

--- client/src/components/Events.tsx
+++ client/src/components/Events.tsx
@@ -1,10 +1,31 @@
-const Events = ({ events }: { events: string[] }) => {
+import { useState } from "react";
+import { type Messages } from "../pages/Room";
+
+const Events = ({ events }: { events: Messages[] }) => {
+    const [showDateIndex, setShowDateIndex] = useState<number | null>(null);
+
+    const toggleDate = (index: number) => {
+        setShowDateIndex(showDateIndex === index ? null : index);
+    };
     return (
-        <ul>
-            {events.map((event, index) => (
-                <li key={index}>{event}</li>
+        <>
+            {events.map((event: Messages, index) => (
+                <div key={index} className="mb-4 text-white">
+                    <p className="mb-2 text-sm">{event.username}</p>
+                    <p
+                        onClick={() => toggleDate(index)}
+                        className="bg-black/50 p-2 px-3 rounded-full inline"
+                    >
+                        {event.message}
+                    </p>
+                    {showDateIndex === index && (
+                        <p className="text-xs mt-2">
+                            {new Date(event.date).toLocaleString()}
+                        </p>
+                    )}
+                </div>
             ))}
-        </ul>
+        </>
     );
 };
 
